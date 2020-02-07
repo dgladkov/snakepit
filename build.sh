dbb197071f8604197330e6bfac551e3c88458eec
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2020-02-06 19:43:27 -0800

    Reogranize repo for Hub builds

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 set -e
 
-docker build ./build -t snakepit-build:latest
+docker build . -t snakepit-build:latest
 
 docker container create --name extract snakepit-build:latest
 trap 'docker container rm -f extract' EXIT
-docker container cp extract:/snakes.tar.gz final/
+docker container cp extract:/snakes.tar.gz .
 
-docker build ./final -t snakepit:latest
+docker build . -t snakepit:latest
